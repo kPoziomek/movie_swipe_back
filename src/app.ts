035294e384c01eccc4780c525df91adfb6c83a2a
Krzysztof Poziomek <krzysztof.poziomek@gmail.com>
2025-01-07 17:33:52 +0100

    Update

--- src/app.ts
+++ src/app.ts
@@ -6,39 +6,23 @@ import { mockUserMiddleware } from "./middleware/mockUser.middleware";
 
 const app = express();
 
-const startServer = async () => {
-  try {
+app.use(
+  cors({
+    origin: ["https://movie-swipe-front-co8ccl3f6.vercel.app", "http://localhost:5173"],
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(mockUserMiddleware);
+app.use(express.urlencoded({ extended: true }));
 
+// Debug middleware
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
 
-    app.use(
-      cors({
-        origin: "http://localhost:5173",
-        credentials: true,
-      })
-    );
-    app.use(express.json());
-    app.use(mockUserMiddleware);
-    app.use(express.urlencoded({ extended: true }));
-
-    // Debug middleware
-    app.use((req, res, next) => {
-      console.log(`${req.method} ${req.url}`);
-      next();
-    });
-
-    app.use("/api/movies", movieRoutes);
-    app.use("/api/users", userRoutes);
-
-    const PORT = process.env.PORT;
-    app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
-    });
-  } catch (error) {
-    console.error("Failed to start server:", error);
-    process.exit(1);
-  }
-};
-
-startServer();
+app.use("/api/movies", movieRoutes);
+app.use("/api/users", userRoutes);
 
 export default app;
