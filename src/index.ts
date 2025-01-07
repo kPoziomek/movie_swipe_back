59f02ee41bd9521f47c8d60fb4b8cad8eaa04534
Krzysztof Poziomek <krzysztof.poziomek@gmail.com>
2025-01-07 17:24:28 +0100

    Update app port

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,8 @@ import dotenv from "dotenv";
 import app from "./app";
 dotenv.config();
 
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
-app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`[server]: Server is running at http://localhost:${PORT}`);
 });
